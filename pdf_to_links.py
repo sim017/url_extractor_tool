db98a97de057c8b43e1d04ef3ef8f329bfb8a28a
sim017 <sim017bawkar@gmail.com>
2019-11-17 18:05:48 +0530

    extracting the urls from text

---
+++ pdf_to_links.py
@@ -0,0 +1,20 @@
+import PyPDF2
+import re
+PDFFile = open("TOIM_2019_11_16.pdf",'rb')
+
+PDF = PyPDF2.PdfFileReader(PDFFile)
+pages = PDF.getNumPages()
+key = '/Annots'
+uri = '/URI'
+ank = '/A'
+urls = []
+
+for page in range(pages):
+    print("Current Page: {}".format(page))
+    pageSliced = PDF.getPage(page)
+    page_content = pageSliced.extractText()
+    #extracting the urls from the texts using regex
+    urls.append(re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', page_content.replace('\n', ' ').replace('\r', '')))
+
+    
+print(urls)
